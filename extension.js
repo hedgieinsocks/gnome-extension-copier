a2f6285f2a4e5505dd1ba83f6b52104efe0616a9
Artem Babii <arthem.babij@gmail.com>
2023-09-15 08:41:42 +0300

    Gnome 45 port (#1)
    refactor: gnome 45 tweaks

--- extension.js
+++ extension.js
@@ -1,31 +1,26 @@
-'use strict';
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
+import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
+import Gio from "gi://Gio";
+import GLib from "gi://GLib";
+import St from "gi://St";
 
-
-const {GLib, St, Gio} = imports.gi;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-
-
-const ICON = 'edit-copy-symbolic';
-
+const ICON = "edit-copy-symbolic";
 
 const ScrollableMenu = class ScrollableMenu extends PopupMenu.PopupMenuSection {
     constructor() {
         super();
-        let scrollView = new St.ScrollView();
+        const scrollView = new St.ScrollView();
         this.innerMenu = new PopupMenu.PopupMenuSection();
         scrollView.add_actor(this.innerMenu.actor);
         this.actor.add_actor(scrollView);
     }
 };
 
-
-class Extension {
-    constructor() {
+export default class CopierExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
         this._indicator = null;
         this._menuId = null;
         this._clipboard = null;
@@ -34,67 +29,75 @@ class Extension {
         this._path = null;
     }
 
-
     _fillMenu(path) {
         this._menu.innerMenu.removeAll();
 
-        let rootPath = this._settings.get_string('path');
+        const rootPath = this._settings.get_string("path");
         if (!rootPath) {
             return;
         }
 
         if (path) {
             if (path !== rootPath) {
-                this._menu.innerMenu.addAction('..',
-                    () => this._fillMenu(path.split('/').slice(0, -1).join('/')),
-                    'go-up-symbolic');
+                this._menu.innerMenu.addAction(
+                    "..",
+                    () => this._fillMenu(path.split("/").slice(0, -1).join("/")),
+                    "go-up-symbolic"
+                );
             }
         } else {
             path = rootPath;
         }
 
-        let items = this._getFiles(path);
+        const items = this._getFiles(path);
 
         if (!items) {
             return;
         }
 
-        items[0].forEach(dir => {
-            this._menu.innerMenu.addAction(dir.get_name(),
+        items[0].forEach((dir) => {
+            this._menu.innerMenu.addAction(
+                dir.get_name(),
                 () => this._fillMenu(`${path}/${dir.get_name()}`),
-                dir.get_icon());
+                dir.get_icon()
+            );
         });
 
-        items[1].forEach(file => {
-            this._menu.innerMenu.addAction(file.get_name(),
+        items[1].forEach((file) => {
+            this._menu.innerMenu.addAction(
+                file.get_name(),
                 () => this._copyFile(`${path}/${file.get_name()}`),
-                file.get_icon());
+                file.get_icon()
+            );
         });
     }
 
-
     _getFiles(path) {
-        let directory = Gio.File.new_for_path(path);
+        const directory = Gio.File.new_for_path(path);
         if (!directory.query_exists(null)) {
             return;
         }
 
-        let enumerator = directory.enumerate_children('standard::name,standard::type,standard::content-type,standard::icon', Gio.FileQueryInfoFlags.NONE, null);
-        let dirs = [];
-        let files = [];
+        const enumerator = directory.enumerate_children(
+            "standard::name,standard::type,standard::content-type,standard::icon",
+            Gio.FileQueryInfoFlags.NONE,
+            null
+        );
+        const dirs = [];
+        const files = [];
 
         while (true) {
-            let fileInfo = enumerator.next_file(null);
+            const fileInfo = enumerator.next_file(null);
             if (!fileInfo) {
                 break;
             }
 
-            let fileType = fileInfo.get_file_type();
+            const fileType = fileInfo.get_file_type();
             if (fileType === Gio.FileType.DIRECTORY) {
                 dirs.push(fileInfo);
             } else if (fileType === Gio.FileType.REGULAR) {
-                let fileMime = fileInfo.get_content_type();
-                if (fileMime.startsWith('text/')) {
+                const fileMime = fileInfo.get_content_type();
+                if (fileMime.startsWith("text/")) {
                     files.push(fileInfo);
                 }
             }
@@ -106,48 +109,41 @@ class Extension {
         return [dirs, files];
     }
 
-
     _copyFile(file) {
         this._indicator.menu.toggle();
-        let data = GLib.file_get_contents(file)[1];
-        let decoder = new TextDecoder();
-        let text = decoder.decode(data).trim();
+        const data = GLib.file_get_contents(file)[1];
+        const decoder = new TextDecoder();
+        const text = decoder.decode(data).trim();
         this._clipboard.set_text(St.ClipboardType.CLIPBOARD, text);
     }
 
-
     _addIndicator() {
-        this._indicator = new PanelMenu.Button(0.0, Me.metadata.name, false);
+        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
 
-        let icon = new St.Icon({
-            gicon: new Gio.ThemedIcon({name: ICON}),
-            style_class: 'popup-menu-icon',
+        const icon = new St.Icon({
+            gicon: new Gio.ThemedIcon({ name: ICON }),
+            style_class: "popup-menu-icon",
         });
         this._indicator.add_child(icon);
 
         this._menu = new ScrollableMenu();
         this._indicator.menu.addMenuItem(this._menu);
+        this._indicator.menu.addAction("Settings", () => this.openPreferences(), "preferences-system-symbolic");
 
-        this._indicator.menu.addAction('Settings',
-            () => ExtensionUtils.openPrefs(),
-            'preferences-system-symbolic');
+        Main.panel.addToStatusArea(this.metadata.name, this._indicator);
 
-        Main.panel.addToStatusArea(Me.metadata.name, this._indicator);
-
-        this._menuId = this._indicator.menu.connect('open-state-changed', open => {
+        this._menuId = this._indicator.menu.connect("open-state-changed", (open) => {
             if (open) {
                 this._fillMenu(null);
             }
         });
     }
 
-
     enable() {
         this._addIndicator();
         this._clipboard = St.Clipboard.get_default();
-        this._settings = ExtensionUtils.getSettings();
+        this._settings = this.getSettings();
     }
-
 
     disable() {
         this._indicator.menu.disconnect(this._menuId);
@@ -156,10 +152,6 @@ class Extension {
         this._menuId = null;
         this._menu = null;
         this._settings = null;
+        this._clipboard = null;
     }
-}
-
-
-function init() {
-    return new Extension();
 }
--- prefs.js
+++ prefs.js
@@ -1,44 +1,33 @@
-'use strict';
+import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
+import Adw from "gi://Adw";
+import Gio from "gi://Gio";
+import Gtk from "gi://Gtk";
 
+export default class CopierPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        const settings = this.getSettings();
+        const page = new Adw.PreferencesPage();
+        const group = new Adw.PreferencesGroup();
+        page.add(group);
 
-const {Adw, Gio, Gtk} = imports.gi;
+        const row = new Adw.ActionRow({
+            title: "Enter Path",
+            subtitle: "Directory with your notes",
+        });
+        group.add(row);
 
-const ExtensionUtils = imports.misc.extensionUtils;
+        const entry = new Gtk.Entry({
+            placeholder_text: "/home/username/mynotes",
+            text: settings.get_string("path"),
+            valign: Gtk.Align.CENTER,
+            hexpand: true,
+        });
 
+        settings.bind("path", entry, "text", Gio.SettingsBindFlags.DEFAULT);
 
-function init() {
-}
+        row.add_suffix(entry);
+        row.activatable_widget = entry;
 
-
-function fillPreferencesWindow(window) {
-    const settings = ExtensionUtils.getSettings();
-
-    const page = new Adw.PreferencesPage();
-    const group = new Adw.PreferencesGroup();
-    page.add(group);
-
-    const row = new Adw.ActionRow({
-        title: 'Enter Path',
-        subtitle: 'Directory with your notes',
-    });
-    group.add(row);
-
-    const entry = new Gtk.Entry({
-        placeholder_text: '/home/username/mynotes',
-        text: settings.get_string('path'),
-        valign: Gtk.Align.CENTER,
-        hexpand: true,
-    });
-
-    row.add_suffix(entry);
-    row.activatable_widget = entry;
-
-    settings.bind(
-        'path',
-        entry,
-        'text',
-        Gio.SettingsBindFlags.DEFAULT
-    );
-
-    window.add(page);
+        window.add(page);
+    }
 }
