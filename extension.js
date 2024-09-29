94af4c5186128f301ddd00e387af1c020b265c20
Artem Babii <arthem.babij@gmail.com>
2024-09-29 10:50:42 +0300

    add gnome 47 support

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
 import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
-import * as Config from 'resource:///org/gnome/shell/misc/config.js';
+import * as Config from "resource:///org/gnome/shell/misc/config.js";
 import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 import St from "gi://St";
@@ -10,157 +10,166 @@ import St from "gi://St";
 const ICON = "edit-copy-symbolic";
 
 const ScrollableMenu = class ScrollableMenu extends PopupMenu.PopupMenuSection {
-    constructor() {
-        super();
-        const scrollView = new St.ScrollView();
-        this.innerMenu = new PopupMenu.PopupMenuSection();
-        const shellVersion = parseFloat(Config.PACKAGE_VERSION).toString().slice(0, 2);
-        if (shellVersion == 45) {
-            scrollView.add_actor(this.innerMenu.actor);
-            this.actor.add_actor(scrollView);
-        } else {
-            scrollView.add_child(this.innerMenu.actor);
-            this.actor.add_child(scrollView);
-        }
+  constructor() {
+    super();
+    const scrollView = new St.ScrollView();
+    this.innerMenu = new PopupMenu.PopupMenuSection();
+    const shellVersion = parseFloat(Config.PACKAGE_VERSION)
+      .toString()
+      .slice(0, 2);
+    if (shellVersion == 45) {
+      scrollView.add_actor(this.innerMenu.actor);
+      this.actor.add_actor(scrollView);
+    } else {
+      scrollView.add_child(this.innerMenu.actor);
+      this.actor.add_child(scrollView);
     }
+  }
 };
 
 export default class CopierExtension extends Extension {
-    constructor(metadata) {
-        super(metadata);
-        this._indicator = null;
-        this._menuId = null;
-        this._clipboard = null;
-        this._settings = null;
-        this._menu = null;
-        this._path = null;
+  constructor(metadata) {
+    super(metadata);
+    this._indicator = null;
+    this._menuId = null;
+    this._clipboard = null;
+    this._settings = null;
+    this._menu = null;
+    this._path = null;
+  }
+
+  _fillMenu(path) {
+    this._menu.innerMenu.removeAll();
+
+    const rootPath = this._settings.get_string("path");
+    if (!rootPath) {
+      return;
     }
 
-    _fillMenu(path) {
-        this._menu.innerMenu.removeAll();
-
-        const rootPath = this._settings.get_string("path");
-        if (!rootPath) {
-            return;
-        }
-
-        if (path) {
-            if (path !== rootPath) {
-                this._menu.innerMenu.addAction(
-                    "..",
-                    () => this._fillMenu(path.split("/").slice(0, -1).join("/")),
-                    "go-up-symbolic"
-                );
-            }
-        } else {
-            path = rootPath;
-        }
-
-        const items = this._getFiles(path);
-
-        if (!items) {
-            return;
-        }
-
-        items[0].forEach((dir) => {
-            this._menu.innerMenu.addAction(
-                dir.get_name(),
-                () => this._fillMenu(`${path}/${dir.get_name()}`),
-                dir.get_icon()
-            );
-        });
-
-        const stripExt = this._settings.get_boolean("strip");
-
-        items[1].forEach((file) => {
-            this._menu.innerMenu.addAction(
-                stripExt ? file.get_name().replace(/\.[^\.]+$/, "") : file.get_name(),
-                () => this._copyFile(`${path}/${file.get_name()}`),
-                file.get_icon()
-            );
-        });
-    }
-
-    _getFiles(path) {
-        const directory = Gio.File.new_for_path(path);
-        if (!directory.query_exists(null)) {
-            return;
-        }
-
-        const enumerator = directory.enumerate_children(
-            "standard::name,standard::type,standard::content-type,standard::icon",
-            Gio.FileQueryInfoFlags.NONE,
-            null
+    if (path) {
+      if (path !== rootPath) {
+        this._menu.innerMenu.addAction(
+          "..",
+          () => this._fillMenu(path.split("/").slice(0, -1).join("/")),
+          "go-up-symbolic",
         );
-        const dirs = [];
-        const files = [];
+      }
+    } else {
+      path = rootPath;
+    }
 
-        while (true) {
-            const fileInfo = enumerator.next_file(null);
-            if (!fileInfo) {
-                break;
-            }
+    const items = this._getFiles(path);
 
-            const fileType = fileInfo.get_file_type();
-            if (fileType === Gio.FileType.DIRECTORY) {
-                dirs.push(fileInfo);
-            } else if (fileType === Gio.FileType.REGULAR) {
-                const fileMime = fileInfo.get_content_type();
-                if (fileMime.startsWith("text/")) {
-                    files.push(fileInfo);
-                }
-            }
+    if (!items) {
+      return;
+    }
+
+    items[0].forEach((dir) => {
+      this._menu.innerMenu.addAction(
+        dir.get_name(),
+        () => this._fillMenu(`${path}/${dir.get_name()}`),
+        dir.get_icon(),
+      );
+    });
+
+    const stripExt = this._settings.get_boolean("strip");
+
+    items[1].forEach((file) => {
+      this._menu.innerMenu.addAction(
+        stripExt ? file.get_name().replace(/\.[^\.]+$/, "") : file.get_name(),
+        () => this._copyFile(`${path}/${file.get_name()}`),
+        file.get_icon(),
+      );
+    });
+  }
+
+  _getFiles(path) {
+    const directory = Gio.File.new_for_path(path);
+    if (!directory.query_exists(null)) {
+      return;
+    }
+
+    const enumerator = directory.enumerate_children(
+      "standard::name,standard::type,standard::content-type,standard::icon",
+      Gio.FileQueryInfoFlags.NONE,
+      null,
+    );
+    const dirs = [];
+    const files = [];
+
+    while (true) {
+      const fileInfo = enumerator.next_file(null);
+      if (!fileInfo) {
+        break;
+      }
+
+      const fileType = fileInfo.get_file_type();
+      if (fileType === Gio.FileType.DIRECTORY) {
+        dirs.push(fileInfo);
+      } else if (fileType === Gio.FileType.REGULAR) {
+        const fileMime = fileInfo.get_content_type();
+        if (fileMime.startsWith("text/")) {
+          files.push(fileInfo);
         }
-
-        enumerator.close(null);
-        dirs.sort((a, b) => a.get_name().localeCompare(b.get_name()));
-        files.sort((a, b) => a.get_name().localeCompare(b.get_name()));
-        return [dirs, files];
+      }
     }
 
-    _copyFile(file) {
-        this._indicator.menu.toggle();
-        const data = GLib.file_get_contents(file)[1];
-        const decoder = new TextDecoder();
-        const text = decoder.decode(data).trim();
-        this._clipboard.set_text(St.ClipboardType.CLIPBOARD, text);
-    }
+    enumerator.close(null);
+    dirs.sort((a, b) => a.get_name().localeCompare(b.get_name()));
+    files.sort((a, b) => a.get_name().localeCompare(b.get_name()));
+    return [dirs, files];
+  }
 
-    _addIndicator() {
-        this._indicator = new PanelMenu.Button(0.5, this.metadata.name, false);
+  _copyFile(file) {
+    this._indicator.menu.toggle();
+    const data = GLib.file_get_contents(file)[1];
+    const decoder = new TextDecoder();
+    const text = decoder.decode(data).trim();
+    this._clipboard.set_text(St.ClipboardType.CLIPBOARD, text);
+  }
 
-        const icon = new St.Icon({
-            gicon: new Gio.ThemedIcon({ name: ICON }),
-            style_class: "popup-menu-icon",
-        });
-        this._indicator.add_child(icon);
+  _addIndicator() {
+    this._indicator = new PanelMenu.Button(0.5, this.metadata.name, false);
 
-        this._menu = new ScrollableMenu();
-        this._indicator.menu.addMenuItem(this._menu);
-        this._indicator.menu.addAction("Settings", () => this.openPreferences(), "preferences-system-symbolic");
+    const icon = new St.Icon({
+      gicon: new Gio.ThemedIcon({ name: ICON }),
+      style_class: "system-status-icon",
+    });
+    this._indicator.add_child(icon);
 
-        Main.panel.addToStatusArea(this.metadata.name, this._indicator);
+    this._menu = new ScrollableMenu();
+    this._indicator.menu.addMenuItem(this._menu);
+    this._indicator.menu.addAction(
+      "Settings",
+      () => this.openPreferences(),
+      "preferences-system-symbolic",
+    );
 
-        this._menuId = this._indicator.menu.connect("open-state-changed", (open) => {
-            if (open) {
-                this._fillMenu(null);
-            }
-        });
-    }
+    Main.panel.addToStatusArea(this.metadata.name, this._indicator);
 
-    enable() {
-        this._addIndicator();
-        this._clipboard = St.Clipboard.get_default();
-        this._settings = this.getSettings();
-    }
+    this._menuId = this._indicator.menu.connect(
+      "open-state-changed",
+      (open) => {
+        if (open) {
+          this._fillMenu(null);
+        }
+      },
+    );
+  }
 
-    disable() {
-        this._indicator.menu.disconnect(this._menuId);
-        this._indicator.destroy();
-        this._indicator = null;
-        this._menuId = null;
-        this._menu = null;
-        this._settings = null;
-        this._clipboard = null;
-    }
+  enable() {
+    this._addIndicator();
+    this._clipboard = St.Clipboard.get_default();
+    this._settings = this.getSettings();
+  }
+
+  disable() {
+    this._indicator.menu.disconnect(this._menuId);
+    this._indicator.destroy();
+    this._indicator = null;
+    this._menuId = null;
+    this._menu = null;
+    this._settings = null;
+    this._clipboard = null;
+  }
 }
--- prefs.js
+++ prefs.js
@@ -4,48 +4,53 @@ import Gio from "gi://Gio";
 import Gtk from "gi://Gtk";
 
 export default class CopierPreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
-        const settings = this.getSettings();
-        const page = new Adw.PreferencesPage();
-        const group = new Adw.PreferencesGroup();
-        page.add(group);
+  fillPreferencesWindow(window) {
+    const settings = this.getSettings();
+    const page = new Adw.PreferencesPage();
+    const group = new Adw.PreferencesGroup();
+    page.add(group);
 
-        // Entry
-        const rowEntry = new Adw.ActionRow({
-            title: "Enter Path",
-            subtitle: "Directory with your notes",
-        });
-        group.add(rowEntry);
+    // Entry
+    const rowEntry = new Adw.ActionRow({
+      title: "Enter Path",
+      subtitle: "Directory with your notes",
+    });
+    group.add(rowEntry);
 
-        const entryPath = new Gtk.Entry({
-            placeholder_text: "/home/username/mynotes",
-            text: settings.get_string("path"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const entryPath = new Gtk.Entry({
+      placeholder_text: "/home/username/mynotes",
+      text: settings.get_string("path"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind("path", entryPath, "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind("path", entryPath, "text", Gio.SettingsBindFlags.DEFAULT);
 
-        rowEntry.add_suffix(entryPath);
-        rowEntry.activatable_widget = entryPath;
+    rowEntry.add_suffix(entryPath);
+    rowEntry.activatable_widget = entryPath;
 
-        // Strip
-        const rowStrip = new Adw.ActionRow({
-            title: "Strip",
-            subtitle: "Hide file extensions",
-        });
-        group.add(rowStrip);
+    // Strip
+    const rowStrip = new Adw.ActionRow({
+      title: "Strip",
+      subtitle: "Hide file extensions",
+    });
+    group.add(rowStrip);
 
-        const toggleStrip = new Gtk.Switch({
-            active: settings.get_boolean("strip"),
-            valign: Gtk.Align.CENTER,
-        });
+    const toggleStrip = new Gtk.Switch({
+      active: settings.get_boolean("strip"),
+      valign: Gtk.Align.CENTER,
+    });
 
-        settings.bind("strip", toggleStrip, "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "strip",
+      toggleStrip,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowStrip.add_suffix(toggleStrip);
-        rowStrip.activatable_widget = toggleStrip;
+    rowStrip.add_suffix(toggleStrip);
+    rowStrip.activatable_widget = toggleStrip;
 
-        window.add(page);
-    }
+    window.add(page);
+  }
 }
